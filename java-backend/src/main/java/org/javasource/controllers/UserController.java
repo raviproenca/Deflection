0863b599015fbcba6aea65dba5c75e8e0a2f15cd
Ravi Proença <raviproenca@gmail.com>
2025-06-16 21:23:23 -0300

    configuring java-spring-boot

---
+++ java-backend/src/main/java/org/javasource/controllers/UserController.java
@@ -0,0 +1,26 @@
+package org.javasource.controllers;
+
+import org.javasource.models.User;
+import org.javasource.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public List<User> lista() {
+        return service.listAll();
+    }
+
+    @PostMapping
+    public User save(@RequestBody User user) {
+        return service.save(user);
+    }
+}
---
+++ java-backend/src/main/java/org/javasource/services/UserService.java
@@ -0,0 +1,22 @@
+package org.javasource.services;
+
+import org.javasource.models.User;
+import org.javasource.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> listAll() {
+        return repository.findAll();
+    }
+
+    public User save(User user) {
+        return repository.save(user);
+    }
+}
